3c700fdd3bf097cdc9e53e32ff2aad512daa345d
rliu99 <rliu99@students.cs.ubc.ca>
2022-05-12 17:47:22 -0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import TodoContainer from "./functionBased/components/TodoContainer"
-import "./functionBased/App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import About from "./functionBased/pages/About"
-import NotMatch from "./functionBased/pages/NotMatch"
-import NavBar from "./functionBased/components/NavBar"
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TodoContainer from "./functionBased/components/TodoContainer";
+import "./functionBased/App.css";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./functionBased/pages/About";
+import NotMatch from "./functionBased/pages/NotMatch";
+import NavBar from "./functionBased/components/NavBar";
 
 export default function App() {
   return (
     <React.StrictMode>
       <Router>
-        <NavBar/>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<TodoContainer />}/>
+          <Route path="/" element={<TodoContainer />} />
           <Route path="about/*" element={<About />} />
-          <Route path="*" element={<NotMatch />} />   
+          <Route path="*" element={<NotMatch />} />
         </Routes>
       </Router>
     </React.StrictMode>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />)
+root.render(<App />);
